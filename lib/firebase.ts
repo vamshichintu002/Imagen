68325f583f857e76f589be16d8beaca0b973be91
vamshichintu002 <vamshichintu002@gmail.com>
2024-10-08 17:55:16 +0530

    firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-export const db = getFirestore(app);
+
+console.log('Firebase config:', firebaseConfig);
