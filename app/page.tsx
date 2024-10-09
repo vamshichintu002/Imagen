b741f822e12bbb5205e3db4244ed14dd783528b6
vamshichintu002 <vamshichintu002@gmail.com>
2024-10-09 10:27:26 +0530

    Added Vercel Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from "@vercel/analytics/react"
 import { IdeogramClone } from "@/components/ideogram-clone"
 
 export default function Page() {
-  return <IdeogramClone />
+  return (
+    <>
+      <Analytics />  {/* This line adds the Analytics component */}
+      <IdeogramClone />
+    </>
+  )
 }
